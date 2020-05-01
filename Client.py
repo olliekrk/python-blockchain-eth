7ef3fb66ea63b12f54e58ac801138fe936ea5e5a
wiktor145 <viktor145@wp.pl>
2020-05-01 13:19:40 +0200

    client update

--- Client.py
+++ Client.py
@@ -2,41 +2,46 @@ import cmd, sys
 from turtle import *
 from app import Web3Connector
 from app import NETWORK_URL
+import contract_deployer
 
 
 class HealthCareShell(cmd.Cmd):
-    intro = 'Welcome to the turtle shell.   Type help or ? to list commands.\n'
+    intro = 'Welcome to the Healthcare shell.   Type help or ? to list commands.\n'
     prompt = '>'
     # file = None
 
     private_key = None
     w3 = None
+    contract = None
+    contact_loader = None
 
-    def _read_file(self, file_name):
+    def _read_key(self, file_name):
         try:
             file = open(file_name)
             self.private_key = file.readline()
         except:
-            print("Could not read a key from " + str(file_name))
+            print("[0][Could not read a key from " + str(file_name) + "]")
 
-    def _login(self):
+    def _connect(self):
         try:
             self.w3 = Web3Connector(NETWORK_URL)
-            print("connection successful!")
+            print("[0][Connection successful]")
             print(self.w3)
         except:
-            print("Connecton error!")
+            print("[1][Connection error]")
 
-
-    def do_login(self, arg):
-        'Login to smart contract using key in provided file'
+    def do_get_key(self, arg):
+        'Get private key from provided file'
         file_name = parse(arg)
         if len(file_name) == 0:
-            print("Not provided any file!")
+            print("[1][Not provided any file]")
         else:
-            self._read_file(file_name[0])
-            if self.private_key:
-                self._login()
+            self._read_key(file_name[0])
+
+    def do_connect(self):
+        'Connect to network'
+        self._connect()
+
 
     def do_add_heartrate(self):
         pass
@@ -49,6 +54,27 @@ class HealthCareShell(cmd.Cmd):
 
     def do_revoke_access(self):
         pass
+
+    def use_contract(self, arg):
+        'Use contract from given file'
+        if not self.w3:
+            print("[1][Not connected]")
+            return
+
+        file_name = parse(arg)
+        if len(file_name) == 0:
+            print("[1][Not provided any file]")
+        else:
+            self._read_contract(file_name[0])
+
+    def _read_contract(self, file_name):
+        try:
+            if not self.contact_loader:
+                self.contact_loader = contract_deployer.ContractLoader(self.w3)
+            self.contract = self.contact_loader.load_by_name(file_name)
+        except:
+            print("[0][Could not read a contract from " + str(file_name) + "]")
+
 
 def parse(arg):
     'Convert a series of zero or more numbers to an argument tuple'
