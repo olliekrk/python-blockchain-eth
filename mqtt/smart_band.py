fe1eadfd0d08099374eded577f52e196ea6c8d4a
olliekrk <krolik.olgierd@gmail.com>
2020-05-02 13:50:46 +0200

    Fixes for smart band mqtt client

--- mqtt/smart_band.py
+++ mqtt/smart_band.py
@@ -9,36 +9,49 @@ import paho.mqtt.client as mqtt
 DEFAULT_HOST = '127.0.0.1'
 DEFAULT_PORT = 1883
 KEEPALIVE = 30
-BACKOFF_INTERVAL = 2
-MEASUREMENT_INTERVAL = 2
-HEART_RATE_TOPIC = 'smart_iot/heart_rate'
+BACKOFF_INTERVAL = 1
+MEASUREMENT_INTERVAL_MAX = 5 # [0, 1) * MAX
+HEART_RATE_TOPIC = 'heart_rate'
 
 """
 Example of IoT Device which measures heart rate of the wearer
 and publishes the data on a appropriate MQTT topic.
 """
 
+def _on_connect(client, userdata, flags, rc):
+    print('Connection success!' if rc == 0 else f'Connection failure: ({rc})')
+
+def _on_disconnect(client, userdata, rc):
+    print(f'Disconnected. ({rc})')
+    
+def _on_publish(client, userdata, mid):
+    print(f'Published message no. {mid}!')
+
 class SmartBand(mqtt.Client):
     def __init__(self, host=DEFAULT_HOST, port=DEFAULT_PORT):
         super().__init__(f'SmartBand_{int(time.time())}')
-        self.connect(host, port, KEEPALIVE)
-        
+        self.on_connect = _on_connect
+        self.on_disconnect = _on_disconnect
+        self.on_publish = _on_publish
+        self.connect_async(host, port, KEEPALIVE)
+    
+    
     @backoff.on_exception(backoff.constant, Exception, interval=BACKOFF_INTERVAL)
     async def start_measurement(self):
         """Starts receiving measurement data and publishing it to the MQTT topic"""
         try:
             for measurement in SmartBand._heart_rate_measurement():
                 self._publish_measurement(measurement)
-                await asyncio.sleep(MEASUREMENT_INTERVAL)
+                await asyncio.sleep(random.random() * MEASUREMENT_INTERVAL_MAX)
         except Exception as e:
-            print(e)
-            print(f'Exception: {type(e)} on publishing message, reconnecting after {BACKOFF_INTERVAL} second(s)')
+            print(str(e))
+            print(f'Exception: {type(e)} on publishing message, retrying after {BACKOFF_INTERVAL} second(s)')
             raise e
     
     def _publish_measurement(self, measurement):
         message = json.dumps(measurement)
-        message_info = self.publish(HEART_RATE_TOPIC, message)
-        print(f'Published: {message}')
+        message_info = self.publish(topic=HEART_RATE_TOPIC, payload=message, qos=1)
+        print(f'Publishing... {message}')
     
     @classmethod
     def _heart_rate_measurement(cls):
@@ -62,6 +75,7 @@ def run():
         sys.exit(1)
         
     try:
+        band.loop_start()
         loop = asyncio.get_event_loop()
         loop.run_until_complete(band.start_measurement())
     except KeyboardInterrupt:
@@ -70,6 +84,7 @@ def run():
         print('Smart band has encountered an error.:', str(e))
     finally:
         band.disconnect()
+        band.loop_stop()
         loop.close()
     
 if __name__ == '__main__':
